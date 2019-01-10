a0c26a05e5f85d8e8f756c46d336d52a3b90e9c7
qqyemingqq <qqyemingqq@gmail.com>
2019-01-10 22:50:20 +0800

    first commit

---
+++ js/ui.js
@@ -0,0 +1,94 @@
+(function (window, document) {
+
+    let layout = document.getElementById('layout'),
+        menu = document.getElementById('menu'),
+        menuLink = document.getElementById('menuLink'),
+        content = document.getElementById('main'),
+        mainButton = document.getElementById('mainButton'),
+        ConfDaoLiangView = document.getElementById('ConfDaoLiang'),
+
+        ConfDaoLiangButton = document.getElementById('ConfDaoLiangButton'),
+        ConfHaiBaoButton = document.getElementById('ConfHaiBaoButton'),
+        addConfDaoLiangButton = document.getElementById('addConfDaoLiangButton')
+
+    ;
+
+    let views = [content, ConfDaoLiangView];
+
+
+    addConfDaoLiangButton.addEventListener('pointerup', () => {
+        console.log("addConfDaoLiangButton");
+
+    });
+
+    ConfDaoLiangButton.addEventListener('pointerup', () => {
+        console.log("ConfDaoLiangButton");
+        switchViw(ConfDaoLiangView);
+    });
+
+    mainButton.addEventListener('pointerup', () => {
+        console.log("mainButton");
+        switchViw(content);
+    });
+
+    ConfHaiBaoButton.addEventListener('pointerup', () => {
+        console.log("ConfHaiBaoButton");
+    });
+
+
+    function toggleClass(element, className) {
+        console.log(element, className);
+        var classes = element.className.split(/\s+/),
+            length = classes.length,
+            i = 0;
+
+
+        for (; i < length; i++) {
+            if (classes[i] === className) {
+                classes.splice(i, 1);
+                break;
+            }
+        }
+        // The className is not found
+        if (length === classes.length) {
+            classes.push(className);
+        }
+
+        element.className = classes.join(' ');
+    }
+
+    function toggleAll(e) {
+        var active = 'active';
+
+        e.preventDefault();
+        toggleClass(layout, active);
+        toggleClass(menu, active);
+        toggleClass(menuLink, active);
+        console.log('menuLink');
+    }
+
+    menuLink.onclick = function (e) {
+        toggleAll(e);
+        console.log('menuLink');
+    };
+
+    content.onclick = function (e) {
+        if (menu.className.indexOf('active') !== -1) {
+            toggleAll(e);
+        }
+    };
+
+    function switchViw(view) {
+        if (views.indexOf(view) !== -1) {
+            views.forEach((e) => {
+                if (view === e) {
+                    e.style.display = 'unset';
+                } else {
+                    e.style.display = 'none';
+                }
+            })
+        } else {
+
+        }
+    }
+}(this, this.document));
